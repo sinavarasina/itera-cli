66b57d8d54c979d9dfbea3b44d9e5ce5b6fc990b
sina_varasina <varasinafarmadani@gmail.com>
2025-08-18 04:44:53 +0700

    nothing in particulary

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,5 @@
 // im still learning about thread management using tokio,
 // so it currently wip atleast the api are working normally
-// i planned to use rustyline and clap so it UI and task.
-// gonna make it REPL-like but if there's an argument then it just run
+// i planned to use rustyline and clap so it UI task (repl interface) and clap as command parser.
+// gonna make it REPL-like but if there's an argument then it just run single command.
+// the interface would be heavily inspired by some cli apps for example iwctl (iwd), or bluetoothctl.
